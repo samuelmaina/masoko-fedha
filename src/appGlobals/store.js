bd63bf8da7049e4b6684ca1d8350dd3515efa1cf
samuelmayna <samuelmayna@gmail.com>
2022-04-20 14:44:15 +0300

    Added the cryptoNewsApi reducer

--- src/appGlobals/store.js
+++ src/appGlobals/store.js
@@ -1,8 +1,9 @@
 import { configureStore } from "@reduxjs/toolkit";
-import { cryptoApi } from "../services";
+import { cryptoApi, cryptoNewsApi } from "../services";
 
 export default configureStore({
   reducer: {
     [cryptoApi.reducerPath]: cryptoApi.reducer,
+    [cryptoNewsApi.reducerPath]: cryptoNewsApi.reducer,
   },
 });
