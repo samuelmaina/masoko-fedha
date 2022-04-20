83beaaeb6fb525e5369ccd969788e5d29ed33f07
samuelmayna <samuelmayna@gmail.com>
2022-04-20 13:23:35 +0300

    Added a search bar that is working

--- src/components/HomePage.jsx
+++ src/components/HomePage.jsx
@@ -8,19 +8,21 @@ import { Typography, Row, Col, Statistic } from "antd";
 
 import { cryptoApi } from "../services";
 
+import { Cryptocurrencies, News } from "./index";
 const { useGetCryptosQuery } = cryptoApi;
 
 const { Title } = Typography;
 
 function HomePage() {
-  const { data, isFetching } = useGetCryptosQuery();
+  const count = 10;
+  const { data, isFetching } = useGetCryptosQuery(count);
 
   const globalStats = data?.data?.stats;
 
-  if (isFetching) return <div>Loading</div>;
+  if (isFetching) return <div>Loading...</div>;
 
   const cols = [
-    { title: "Total CrypoCurrencies", value: globalStats.total },
+    { title: "Total CrypoCurrencies", value: millify(globalStats.total) },
     { title: "Total Exchanges", value: millify(globalStats.totalExchanges) },
     { title: "Total Market Cap", value: millify(globalStats.totalMarketCap) },
     { title: "Total 24hr Volume", value: millify(globalStats.total24hVolume) },
@@ -32,12 +34,31 @@ function HomePage() {
         Global Crypto Stats
       </Title>
       <Row>{renderColumns(cols)}</Row>
+
+      <div className="home-heading-container">
+        <Title level={2} className="home-title">
+          Top 10 Cryptocurrencies in the World
+        </Title>
+        <Title level={4} className="show-more">
+          <Link to="/cryptocurrencies"> Show more</Link>
+        </Title>
+      </div>
+      <Cryptocurrencies simplified />
+      <div className="home-heading-container">
+        <Title level={2} className="home-title">
+          Latest Crypto News
+        </Title>
+        <Title level={4} className="show-more">
+          <Link to="/news"> Show more</Link>
+        </Title>
+      </div>
+      <News simplified />
     </>
   );
 }
 
 function renderColumns(cols) {
-  return cols.map((col) => {
+  return cols.map((col, index) => {
     const { title, value } = col;
 
     /**
@@ -47,7 +68,7 @@ function renderColumns(cols) {
 
     return (
       <Col span={12}>
-        <Statistic title={title} value={value}></Statistic>
+        <Statistic title={title} value={value} key={index}></Statistic>
       </Col>
     );
   });
