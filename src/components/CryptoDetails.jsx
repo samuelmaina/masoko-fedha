5dbe2376fa37af1cc53a3848bafdf9b2122af5c8
samuelmayna <samuelmayna@gmail.com>
2022-05-18 15:43:34 +0300

    Refactored the CryptoDetails to  use the basic components from the Native folder

--- src/components/CryptoDetails.jsx
+++ src/components/CryptoDetails.jsx
@@ -2,30 +2,28 @@ import React, { useState } from "react";
 import { useParams } from "react-router-dom";
 import HTMLReactParser from "html-react-parser";
 
-import millify from "millify";
-
-import { Typography, Col, Row, Select } from "antd";
-
-import {
-  MoneyCollectOutlined,
-  DollarCircleOutlined,
-  FundOutlined,
-  ExclamationCircleOutlined,
-  StopOutlined,
-  TrophyOutlined,
-  CheckOutlined,
-  NumberOutlined,
-  ThunderboltOutlined,
-} from "@ant-design/icons";
+import { Typography, Col, Row } from "antd";
 
 import { LineChart } from "./index";
 
-import { cryptoApi, localUtils } from "../services";
+import { cryptoApi } from "../services";
+import { basics } from "./Natives";
+import { stats } from "../data";
+
+const { cryptoStats, genericStats } = stats;
+
+const {
+  createTitle,
+  createColumn,
+  createParagraph,
+  createSelect,
+  createOption,
+  createText,
+} = basics;
 
 const { Title, Text } = Typography;
-const { Option } = Select;
+
 const { useGetCryptoDetailsQuery, useGetCoinHistoryQuery } = cryptoApi;
-const { convertToKenyaShillings, displayAsKenyaShilling } = localUtils;
 
 function CryptoDetails() {
   const { coinId } = useParams();
@@ -44,182 +42,142 @@ function CryptoDetails() {
 
   if (isFetching || isFetchingCryptoDetails) return <div> Loading...</div>;
 
-  return (
-    <Col className="coin-detail-container">
+  return createColumn(
+    <>
       {renderHeaderInfo()}
       {renderTimePeriodSelector()}
-      {coinHistory ? (
-        <LineChart
-          coinHistory={coinHistory}
-          currentPrice={cryptoDetails.price}
-          coinName={cryptoDetails.name}
-        />
-      ) : (
-        <div>Loading </div>
-      )}
+      {renderLineChart()}
       {renderStatsContainer()}
       {renderCoinInfo()}
-    </Col>
+    </>,
+    "coin-detail-container"
   );
 
   function renderHeaderInfo() {
-    return (
-      <Col className="coin-heading-container">
-        <Title level={2} className="coin-name">
-          {cryptoDetails.name} {cryptoDetails.slug ? cryptoDetails.slug : ""}
-        </Title>
-        <p>
-          {cryptoDetails.name} live price in Kenya Shilings. View value
-          statistics, market cap and supply.
-        </p>
-      </Col>
+    return createColumn(
+      <>
+        {createTitle(
+          2,
+          `${cryptoDetails.name} ${
+            cryptoDetails.slug ? cryptoDetails.slug : " "
+          }`,
+          "coin-name"
+        )}
+        {createParagraph(`${cryptoDetails.name} live price in Kenya Shilings. View value
+        statistics, market cap and supply.`)}
+      </>,
+      "coin-heading-container"
     );
   }
 
   function renderTimePeriodSelector() {
     const time = ["3h", "24h", "7d", "30d", "3m", "1y", "5y"];
     const defaultTime = "Select Time Period";
-    return (
-      <Select
-        defaultValue={timePeriod}
-        className="select-timeperiod"
-        placeholder="Select Time Period"
-        onChange={(period) => {
-          if (period === defaultTime) return setTimePeriod("7d");
-          setTimePeriod(period);
-        }}
-      >
-        <Option value={defaultTime}>{defaultTime}</Option>
-        {time.map((date, index) => (
-          <Option key={index} value={date}>
-            {date}
-          </Option>
-        ))}
-      </Select>
+
+    const onChange = (period) => {
+      if (period === defaultTime) return setTimePeriod("7d");
+      setTimePeriod(period);
+    };
+
+    return createSelect(
+      <>
+        {createOption(defaultTime)}
+        {time.map((date, index) => createOption(date, index))}
+      </>,
+      "Select Time Period",
+      onChange,
+      timePeriod,
+      "select-timeperiod"
+    );
+  }
+
+  function renderLineChart() {
+    return coinHistory ? (
+      <LineChart
+        coinHistory={coinHistory}
+        currentPrice={cryptoDetails.price}
+        coinName={cryptoDetails.name}
+      />
+    ) : (
+      <div>Loading </div>
     );
   }
 
   function renderStatsContainer() {
-    return (
-      <Col className="stats-container">
+    return createColumn(
+      <>
         {renderCoinStats()}
         {renderGlobalStats()}
-      </Col>
+      </>,
+      "stats-container"
     );
-    function renderCoinStats() {
-      const stats = [
-        {
-          title: "Price to Kenya Shilling",
-          value: ` ${
-            cryptoDetails?.price && displayAsKenyaShilling(cryptoDetails?.price)
-          }`,
-          icon: <DollarCircleOutlined />,
-        },
-        { title: "Rank", value: cryptoDetails?.rank, icon: <NumberOutlined /> },
-        {
-          title: "24h Volume",
-          value: `${
-            cryptoDetails?.["24hVolume"] &&
-            displayAsKenyaShilling(cryptoDetails?.["24hVolume"])
-          }`,
-          icon: <ThunderboltOutlined />,
-        },
-        {
-          title: "Market Cap",
-          value: `${
-            cryptoDetails?.marketCap &&
-            displayAsKenyaShilling(cryptoDetails?.marketCap)
-          }`,
-          icon: <DollarCircleOutlined />,
-        },
-        {
-          title: "All-time-high(daily avg.)",
-          value: `${
-            cryptoDetails?.allTimeHigh?.price &&
-            displayAsKenyaShilling(cryptoDetails.allTimeHigh.price)
-          }`,
-          icon: <TrophyOutlined />,
-        },
-      ];
-      return (
-        <Col className="coin-value-statistics">
-          <Col className="coin-value-statistics-heading">
-            <Title level={3} className="coin-details-heading">
-              {cryptoDetails.name} Value Statistics
-            </Title>
-            <p> An overview showing the stats of {cryptoDetails.name}</p>
-            {stats.map(({ icon, title, value }) => (
-              <Col className="coin-stats">
-                <Col className="coin-stats-name">
-                  <Text key={1}>{icon}</Text>
-                  <Text key={2}>{title}</Text>
-                </Col>
-                <Text className="stats">{value} </Text>
-              </Col>
-            ))}
-          </Col>
-        </Col>
-      );
-    }
+  }
+  function renderCoinStats() {
+    const stats = cryptoStats(cryptoDetails);
+    return createColumn(
+      createColumn(
+        <>
+          {createTitle(
+            2,
+            `${cryptoDetails.name} Value Statistics`,
+            "coin-details-heading"
+          )}
+          {createParagraph(
+            `An overview showing the stats of ${cryptoDetails.name}`
+          )}
 
-    function renderGlobalStats() {
-      const genericStats = [
-        {
-          title: "Number Of Markets",
-          value: cryptoDetails?.numberOfMarkets,
-          icon: <FundOutlined />,
-        },
-        {
-          title: "Number Of Exchanges",
-          value: cryptoDetails?.numberOfExchanges,
-          icon: <MoneyCollectOutlined />,
-        },
-        {
-          title: "Aprroved Supply",
-          value: cryptoDetails?.supply?.confirmed ? (
-            <CheckOutlined />
-          ) : (
-            <StopOutlined />
-          ),
-          icon: <ExclamationCircleOutlined />,
-        },
-        {
-          title: "Total Supply",
-          value: `${
-            cryptoDetails?.supply?.total &&
-            displayAsKenyaShilling(cryptoDetails?.supply?.total)
-          }`,
-          icon: <ExclamationCircleOutlined />,
-        },
-        {
-          title: "Circulating Supply",
-          value: `${
-            cryptoDetails?.supply?.circulating &&
-            displayAsKenyaShilling(cryptoDetails?.supply?.circulating)
-          }`,
-          icon: <ExclamationCircleOutlined />,
-        },
-      ];
-      return (
-        <Col className="other-stats-info">
-          <Col className="coin-value-statistics-heading">
-            <Title level={3} className="coin-details-heading">
-              Other Value Statistics
-            </Title>
-            <p> An overview showing the stats of all other cryptoCurrencies</p>
-            {genericStats.map(({ icon, title, value }) => (
-              <Col className="coin-stats">
-                <Col className="coin-stats-name">
-                  <Text key={1}>{icon}</Text>
-                  <Text key={2}>{title}</Text>
-                </Col>
-                <Text className="stats">{value} </Text>
+          {displayStats(stats)}
+        </>,
+        "coin-value-statistics-heading"
+      ),
+      "coin-value-statistics"
+    );
+  }
+
+  function renderGlobalStats() {
+    const stats = genericStats(cryptoDetails);
+
+    return createColumn(
+      createColumn(
+        <>
+          {createTitle(3, "Other Value Statistics", "coin-details-heading")}
+          {createParagraph(
+            "An overview showing the stats of all other cryptoCurrencies"
+          )}
+
+          {stats.map(({ icon, title, value }) => (
+            <Col className="coin-stats">
+              <Col className="coin-stats-name">
+                <Text key={1}>{icon}</Text>
+                <Text key={2}>{title}</Text>
               </Col>
-            ))}
-          </Col>
-        </Col>
-      );
-    }
+              {/* <Text className="stats">{value} </Text> */}
+            </Col>
+          ))}
+        </>,
+        "coin-value-statistics-heading"
+      ),
+      "other-stats-info"
+    );
+  }
+
+  function displayStats(stats) {
+    return stats.map(
+      ({ icon, title, value }) =>
+        createColumn(
+          <>
+            {createColumn(
+              <>
+                {createText(icon)}
+                {createText(title)}
+              </>,
+              "coin-stats-name"
+            )}
+            {createText(value, "stats")}
+          </>
+        ),
+      "coin-stats"
+    );
   }
   function renderCoinInfo() {
     return (
