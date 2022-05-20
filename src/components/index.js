95c13d57559ef0d0f86ce1d825b58214e99c4518
samuelmayna <samuelmayna@gmail.com>
2022-05-20 05:49:28 +0300

    included the  Routes

--- src/components/index.js
+++ src/components/index.js
@@ -1,4 +1,5 @@
 export { default as Navbar } from "./Navbar";
+export { default as Routes } from "./Routes";
 export { default as Exchanges } from "./Exchanges";
 export { default as HomePage } from "./HomePage";
 export { default as Cryptocurrencies } from "./Cryptocurrencies";
